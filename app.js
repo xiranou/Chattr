4c57d86c003fb9de0ae52ec071572c547330aa90
xiranou <xiranou@gmail.com>
2015-12-15 11:46:35 -0500

    move socket.io code to its own file

--- app.js
+++ app.js
@@ -1,5 +1,3 @@
-
-
 var express = require('express'),
   config = require('./config/config');
 
@@ -13,10 +11,4 @@ server.listen(config.port, function () {
   console.log('Express server listening on port ' + config.port);
 });
 
-var io = require('socket.io')(server);
-
-io.on('connection', function (socket) {
-  socket.on('chat-msg', function(msg) {
-    socket.broadcast.emit('chat-msg', msg);
-  });
-});
+var io = require('./lib/sockets').listen(server);
