f4f3a2ada975127c93e7394e364e404051048e06
xiranou <xiranou@gmail.com>
2015-12-17 12:50:35 -0500

    initial user-list with nickname

--- public/js/client.js
+++ public/js/client.js
@@ -1,7 +1,22 @@
 $(document).ready(function() {
     var $input = $('.user-input'),
-        $chatBox = $('.chat-box');
+        $chatBox = $('.chat-box'),
+        $userListC = $('.user-list-c'),
+        $userList = $userListC.find(".user-list");
+
     var socket = io();
+
+    var newUser = $.Deferred();
+    var nickname;
+
+    getNickname();
+
+    socket.on('new-user-joined', appendUser);
+
+    newUser.promise().then(function (result) {
+        nickname = result.nickname;
+        socket.emit("nickname-setted", result);
+    });
 
     $input.keydown(function(e) {
         if (e.keyCode === 13) {
@@ -24,9 +39,27 @@ $(document).ready(function() {
         options = options || {};
         var klass = options.selfClass ? "self-msg" : "client-msg";
         $('<p>')
-            .text(msg)
+            .text(nickname + ": " + msg)
             .addClass('msg')
             .addClass(klass)
             .appendTo($chatBox);
+    }
+
+    function getNickname (promptMsg) {
+        promptMsg = promptMsg || "Please enter your nickname.";
+        var name = prompt(promptMsg);
+
+        if (name) {
+            newUser.resolve({nickname: name});
+            return name;
+        } else {
+            getNickname("You must enter a nickname.");
+        }
+    }
+
+    function appendUser (user) {
+        $('<li>')
+            .text(user.nickname)
+            .appendTo($userList);
     }
 });
