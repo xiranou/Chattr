3bf6d31fd3709ba144db3104447f1efe9a992818
xiranou <xiranou@gmail.com>
2015-12-18 16:02:46 -0500

    breakup tasks to different files; separate devDependencies again

---
+++ tasks/prod.js
@@ -0,0 +1,35 @@
+var plumber = require('gulp-plumber'),
+    rename = require('gulp-rename'),
+    sass = require('gulp-sass'),
+    uglify = require('gulp-uglify'),
+    concat = require('gulp-concat');
+
+module.exports = function (gulp, config) {
+  var paths = config.paths;
+
+  gulp.task('build-js', function () {
+    return gulp.src(paths.js)
+      .pipe(concat('application.js'))
+      .pipe(uglify())
+      .pipe(gulp.dest('./public/js'));
+  });
+
+  gulp.task('build-vendor', function () {
+    return gulp.src(paths.components)
+      .pipe(concat('vendor.js'))
+      .pipe(gulp.dest('./public/js'));
+  });
+
+  gulp.task('build-css', function () {
+    return gulp.src(paths.css)
+      .pipe(sass({outputStyle: 'compressed'}))
+      .pipe(rename('application.css'))
+      .pipe(gulp.dest('./public/css'));
+  });
+
+  gulp.task('build:prod', [
+    'build-js',
+    'build-vendor',
+    'build-css'
+  ]);
+};
