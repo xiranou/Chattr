8d3286ba6488bac0660064b6d31b62ad31179db4
xiranou <xiranou@gmail.com>
2015-12-18 17:34:13 -0500

    better nickname setting

--- public/js/client.js
+++ public/js/client.js
@@ -1,74 +1,89 @@
 $(document).ready(function() {
-    var $input = $('.user-input'),
+    var $body = $('body'),
+        $input = $('.user-input'),
         $chatBox = $('.chat-box'),
         $userListC = $('.user-list-c'),
         $userList = $(".user-list"),
-        $welcomeText = $('.welcome-text');
+        $welcomeText = $('.welcome-text'),
+        $nicknameC = $('.nickname-c'),
+        $userNickname = $('.user-nickname');
 
     var socket = io(window.location.host + "/user");
 
-    var newUser = $.Deferred();
-
-    var currentUser;
+    var currentUser = {};
 
     socket
-        .on('client-connected', initializeUser)
+        .on('client-connected', initializeClient)
         .on('client-disconnected', appendClients)
         .on('new-user-joined', appendUser)
         .on('chat-msg', appendMsg);
 
-    $input.keydown(function(e) {
+    $userNickname.keydown(getNickname);
+    $input.keydown(getMsg);
+
+    function getNickname (e) {
         if (e.keyCode === 13) {
+            var name = $userNickname.val();
+            if (name) {
+                currentUser.nickname = name;
+                $nicknameC.hide();
+                updateWelcomeText();
+                appendAnnouncement(name + " has joined", 'join');
+                socket.emit('nickname-setted', currentUser);
+                $input.prop('disabled', false).focus();
+            } else {
+                alert("Nickname can't be blank");
+            }
+        }
+    }
 
+    function getMsg (e) {
+        if (e.keyCode === 13) {
             var chatMsg = $input.val();
-
             socket.emit('chat-msg', {user: currentUser, msg: chatMsg});
-
             appendMsg({user: currentUser, msg: chatMsg});
-
             $input.val('');
         }
-    });
+    }
 
     function appendMsg (userMsg) {
         var klass = userMsg.user === currentUser ? "self-msg" : "client-msg";
         var name = userMsg.user.nickname;
         var msg = userMsg.msg;
-        $('<p>')
-            .text(name + ": " + msg)
-            .addClass('msg')
-            .addClass(klass)
-            .appendTo($chatBox);
-        $chatBox[0].scrollTop = $chatBox[0].scrollHeight;
+        createChatMsg(msg, klass, name);
     }
 
-    function getNickname (promptMsg) {
-        promptMsg = promptMsg || "Please enter your nickname.";
-        var name = prompt(promptMsg);
-
-        if (name) {
-            return name;
-        } else {
-            return getNickname("You must enter a nickname.");
+    function appendAnnouncement (msg, type) {
+        switch (type){
+          case 'join':
+            createChatMsg(msg, 'announcement');
+            break;
+          default:
+            break;
         }
     }
 
-    function initializeUser (response) {
-        var name = getNickname();
-        newUser.resolve({nickname: name, socketId: response.socketId});
-        newUser.promise().then(function (result) {
-            currentUser = result;
-            appendClients(response.clients);
-            socket.emit("nickname-setted", currentUser);
-        });
+    function createChatMsg (msg, klass, userName) {
+        if (userName) {
+            msg = userName + ": " + msg;
+        }
 
-        updateWelcomeText();
+        $('<p>')
+            .text(msg)
+            .addClass('msg')
+            .addClass(klass)
+            .appendTo($chatBox);
+
+        $chatBox[0].scrollTop = $chatBox[0].scrollHeight;
+    }
+
+    function initializeClient (response) {
+        currentUser.socketId = response.socketId;
+        appendClients(response.clients);
     }
 
     function appendClients (clients) {
-        var users = getConnectedUsers(clients);
-
-        appendUserList(users);
+        appendUserList(getConnectedUsers(clients));
     }
 
     function updateWelcomeText () {
