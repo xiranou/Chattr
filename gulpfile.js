8b48b19af29ca142314d362e2e6004fa5e0f328d
xiranou <xiranou@gmail.com>
2015-12-30 17:09:01 -0500

    include socket.io-client to concat application.js for minifying file

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@ var gulp = require('gulp');
 var config = {
     paths: {
                 js: [
+                        './node_modules/socket.io/node_modules/socket.io-client/socket.io.js',
                         './public/components/jquery/dist/jquery.min.js',
                         './public/components/lodash/lodash.min.js',
                         './public/js/**/*.js',
