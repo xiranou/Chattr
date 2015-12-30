70f7e6852a6637bc77069306f63af659be2dd12c
xiranou <xiranou@gmail.com>
2015-12-30 11:57:44 -0500

    initial private-chat function

--- public/js/client.js
+++ public/js/client.js
@@ -2,7 +2,6 @@ $(document).ready(function() {
     var $body = $('body'),
         $userInput = $('.user-input'),
         $chatBox = $('.chat-box'),
-        $userListC = $('.user-list-c'),
         $userList = $(".user-list"),
         $welcomeText = $('.welcome-text'),
         $nicknameC = $('.nickname-c'),
@@ -16,11 +15,16 @@ $(document).ready(function() {
         .on('client-connected', initializeClient)
         .on('client-disconnected', disconnectClient)
         .on('new-user-joined', newUserJoined)
-        .on('chat-msg', appendMsg)
-        .on('user-inputting', userIsTyping);
+        .on('chat-msg', createChatMsg)
+        .on('user-inputting', highlightTypingUser)
+        .on('private-msg', createChatMsg);
 
     $userNickname.keyup(getNickname);
     $userInput.keyup(typingInput);
+
+    $userList.on('mousedown', 'li', selectPrivateChat);
+
+    $userInput.on('private-chat-selected', enablePrivateChat);
 
     function getNickname (e) {
         if (e.which === 13) {
@@ -39,15 +43,21 @@ $(document).ready(function() {
     function typingInput (e) {
         if (e.which === 13) {
             var chatMsg = $userInput.val();
-            socket.emit('chat-msg', {user: currentUser, msg: chatMsg});
-            appendMsg({user: currentUser, msg: chatMsg});
+            var targetSid = $userInput.data().targetSid;
+
+            createChatMsg({
+                user: currentUser,
+                msg: chatMsg,
+                targetSid: targetSid
+            });
             $userInput.val('');
+            socket.emit('chat-msg', {user: currentUser, msg: chatMsg, targetSid: targetSid});
         }
 
         socket.emit('user-inputting', {user: currentUser, hasInput: $userInput.val() !== ''});
     }
 
-    function userIsTyping (userTyping) {
+    function highlightTypingUser (userTyping) {
         var $userTags = $userList.find('li');
         var typingUserTag = _.filter($userTags, function (tag) {
             return $(tag).data().sId === userTyping.user.socketId;
@@ -55,18 +65,20 @@ $(document).ready(function() {
         $(typingUserTag).toggleClass('typing', userTyping.hasInput);
     }
 
-    function appendMsg (userMsg) {
-        var klass = userMsg.user === currentUser ? "self-msg" : "client-msg";
-        var name = userMsg.user.nickname;
-        var msg = userMsg.msg;
-        createChatMsg(msg, klass, name);
-    }
+    function createChatMsg (userMsg) {
+        var isSelf = (userMsg.user === currentUser);
+        var klass = isSelf ? "self-msg" : "client-msg";
+        var name = isSelf ? "You" : userMsg.user.nickname;
+        var msg = name + ": " + userMsg.msg;
 
-    function createChatMsg (msg, klass, userName) {
-        if (userName) {
-            msg = userName + ": " + msg;
+        if (userMsg.targetSid) {
+            klass = klass + " private";
         }
 
+        appendChatMsg(msg, klass);
+    }
+
+    function appendChatMsg (msg, klass) {
         $('<p>')
             .text(msg)
             .addClass('msg')
@@ -84,12 +96,12 @@ $(document).ready(function() {
     function newUserJoined (user) {
         appendUser(user);
         var announcementMsg = user.nickname + " has joined";
-        createChatMsg(announcementMsg, 'announcement', null);
+        appendChatMsg(announcementMsg, 'announcement');
     }
 
     function disconnectClient (response) {
         var msg = response.disClient.nickname + " " + "has left Chattr :(";
-        createChatMsg(msg, "announcement", null);
+        appendChatMsg(msg, "announcement");
         appendClients(response.clients);
     }
 
@@ -130,5 +142,37 @@ $(document).ready(function() {
         } else {
             return false;
         }
+    }
+
+    function selectPrivateChat (e) {
+        e.preventDefault();
+        var $userTag = $(e.target);
+        var $previouslySelected = $('.chat-selected');
+        var sId = $userTag.data().sId;
+
+        if ($userTag[0] === $previouslySelected[0]) {
+            $userTag.toggleClass('chat-selected');
+        } else {
+            $previouslySelected.toggleClass('chat-selected');
+            $userTag.toggleClass('chat-selected');
+        }
+
+        $userInput.trigger('private-chat-selected', [$userTag]);
+    }
+
+    function enablePrivateChat (e, $selectedUserTag) {
+        e.preventDefault();
+        // toggle checks if any userTag is selected
+        var toggle = $('.chat-selected')[0] !== undefined;
+        var placeholderMsg =  toggle ?
+            ('to ' + $selectedUserTag.text() + '...') : ('type here...');
+        var targetSid = toggle ?
+            $selectedUserTag.data().sId : null;
+
+        $userInput
+            .toggleClass('private-chat', toggle)
+            .attr('placeholder', placeholderMsg)
+            .data('targetSid', targetSid)
+            .focus();
     }
 });
